97618df4693802ed2f7b3e4dd4248fdd89acce1f
nqui <nqui@users.noreply.github.com>
2020-02-27 15:47:04 -0500

    add function to grab api key from secrets folder

--- get_ssh_keys.py
+++ get_ssh_keys.py
@@ -1,7 +1,12 @@
 import json
 import requests
 
-api_token = ''
+def get_api_token(filename):
+    with open(filename, 'r') as f:
+        api_token = f.readline().rstrip('\n')
+    return api_token
+
+api_token = get_api_token('/Users/jeje/Development/python/do-api/secrets/api_key.txt')
 api_url_base = 'https://api.digitalocean.com/v2/'
 headers = {'Content-Type': 'application/json',
            'Authorization': 'Bearer {0}'.format(api_token)}
